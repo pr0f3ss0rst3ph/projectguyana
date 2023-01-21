31385da1ba19f932be1784206700f816e2775d81
Stephon Fraser <43830615+pr0f3ss0rst3ph@users.noreply.github.com>
2023-01-21 10:34:35 -0400

    Count Removed from app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <div className="App">
